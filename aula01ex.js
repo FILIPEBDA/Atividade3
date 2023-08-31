a5550d7f8263bf6b734011780fcc9845f94c5187
FILIPEBDA <143577668+FILIPEBDA@users.noreply.github.com>
2023-08-31 19:00:51 -0300

    Adicionando atividade 1

---
+++ aula01ex.js
@@ -0,0 +1,12 @@
+
+const salario = 1320
+const aumento = 0.05
+const novosalario =salario+(salario*aumento)
+
+
+console.log(`Salário Antigo: ${salario}`)
+console.log(`Salário Novo: ${novosalario}`)
+
+
+
+
